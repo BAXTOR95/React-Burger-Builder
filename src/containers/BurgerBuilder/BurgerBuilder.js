cfd82a6215c99496b163e2f3b0bb88f45bc8a517
Brian Arriaga <brian.arriaga@gmail.com>
2021-01-23 14:20:45 -0400

    Adding checkpoint

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 1,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients) {
@@ -70,6 +71,18 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({ purchasing: true });
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -79,8 +92,13 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal>
-                    <OrderSummary ingredients={ this.state.ingredients } />
+                <Modal show={ this.state.purchasing } modalClosed={ this.purchaseCancelHandler }>
+                    <OrderSummary
+                        ingredients={ this.state.ingredients }
+                        price={ this.state.totalPrice }
+                        purchaseCancelled={ this.purchaseCancelHandler }
+                        purchaseContinued={ this.purchaseContinueHandler }
+                    />
                 </Modal>
                 <Burger ingredients={ this.state.ingredients } />
                 <BuildControls
@@ -88,6 +106,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={ this.removeIngredientHandler }
                     disabled={ disabledInfo }
                     purchasable={ this.state.purchasable }
+                    ordered={ this.purchaseHandler }
                     price={ this.state.totalPrice }
                 />
             </Aux>
